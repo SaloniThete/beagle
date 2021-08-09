2e695ba22038a7672fecfbbdd05a4e23a6a6ac07
Frederico Noce <frederico.noce@zup.com.br>
2021-08-09 15:02:56 -0300

    implement BeagleImplicitContextSerializer

---
+++ backend/framework/src/main/kotlin/br/com/zup/beagle/serialization/jackson/BeagleImplicitContextSerializer.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.serialization.jackson
+
+import br.com.zup.beagle.annotation.ImplicitContext
+import com.fasterxml.jackson.core.JsonGenerator
+import java.lang.reflect.ParameterizedType
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.javaField
+import kotlin.reflect.jvm.javaType
+
+internal fun serializeImplicitContexts(bean: Any, generator: JsonGenerator) {
+    val implicitContexts = findImplicitContexts(bean)
+    for (implicitContext in implicitContexts) {
+        serializeAsImplicitContext(implicitContext, generator)
+    }
+}
+
+private fun serializeAsImplicitContext(implicitContext: MutableMap.MutableEntry<String, Any?>, generator: JsonGenerator) {
+    generator.writeFieldName(implicitContext.key)
+    generator.writeObject(implicitContext.value)
+}
+
+private fun findImplicitContexts(bean: Any?): HashMap<String, Any?> {
+    val implicitContexts = HashMap<String, Any?>()
+    bean?.let {
+        for (property in bean::class.memberProperties) {
+            property.isAccessible = true
+            property.findAnnotation<ImplicitContext>()?.let {
+                implicitContexts.put(property.name, resolveMethod(bean, property))
+            }
+        }
+    }
+
+    return implicitContexts
+}
+
+private fun resolveMethod(bean: Any?, property: KProperty1<out Any, *>): Any? {
+    val params = property.returnType.javaType as ParameterizedType
+    val inputClass = (params.actualTypeArguments[0] as Class<*>)
+    val fieldDefinition = property.javaField
+    fieldDefinition?.isAccessible = true
+
+    val fieldValue = fieldDefinition?.get(bean)
+    val myMethod = fieldValue?.javaClass?.getDeclaredMethod("invoke", inputClass)
+    myMethod?.isAccessible = true
+
+    val id = generateImplicitContextId(property)
+    val values = inputClass.kotlin.primaryConstructor
+        ?.parameters
+        ?.associate { it to (if (it.name == "contextId") id else null) }
+        ?: error("parameters not found")
+
+    val inputParam = inputClass.kotlin.primaryConstructor?.callBy(values)
+    return myMethod?.invoke(fieldValue, inputParam)
+}
+
+private fun generateImplicitContextId(property: KProperty1<out Any, *>): String {
+    return property.findAnnotation<ImplicitContext>()?.id?.let {
+        if (it.isEmpty())
+            property.name
+        else
+            it.replace(" ", "")
+    } ?: run {
+        property.name
+    }
+}
--- backend/framework/src/main/kotlin/br/com/zup/beagle/serialization/jackson/BeagleTypeSerializer.kt
+++ backend/framework/src/main/kotlin/br/com/zup/beagle/serialization/jackson/BeagleTypeSerializer.kt
@@ -16,22 +16,11 @@
 
 package br.com.zup.beagle.serialization.jackson
 
-import br.com.zup.beagle.annotation.ImplicitContext
 import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter
 import com.fasterxml.jackson.databind.ser.impl.ObjectIdWriter
 import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase
-import java.io.IOException
-import java.lang.reflect.ParameterizedType
-import kotlin.reflect.KProperty1
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.jvm.isAccessible
-import kotlin.reflect.jvm.javaField
-import kotlin.reflect.jvm.javaType
 
 internal class BeagleTypeSerializer : BeanSerializerBase {
 
@@ -87,94 +76,12 @@ internal class BeagleTypeSerializer : BeanSerializerBase {
         generator.writeStartObject()
         getBeagleType(bean::class.java, this.classLoader)
             ?.also { (key, value) -> generator.writeStringField(key, value) }
-        serializeFields(bean, generator, provider)
+        super.serializeFields(bean, generator, provider)
+        serializeImplicitContexts(bean, generator)
         generator.writeEndObject()
     }
 
     private fun setup(classLoader: ClassLoader = BeagleTypeSerializer::class.java.classLoader) {
         this.classLoader = classLoader
-    }
-
-    override fun serializeFields(bean: Any?, gen: JsonGenerator?, provider: SerializerProvider) {
-        val implicitContexts = findImplicitContexts(bean)
-        val props: Array<BeanPropertyWriter> = if (_filteredProps != null && provider.activeView != null) {
-            _filteredProps
-        } else {
-            _props
-        }
-        var i = 0
-        try {
-            val len = props.size
-            while (i < len) {
-                val prop = props[i]
-                if (prop != null && !implicitContexts.contains(prop.name)) { // can have nulls in filtered list
-                    prop.serializeAsField(bean, gen, provider)
-                } else {
-                    val actions = implicitContexts[prop.name]
-                    gen?.writeFieldName(prop.name)
-                    gen?.writeObject(actions)
-                }
-                ++i
-            }
-            _anyGetterWriter?.getAndSerialize(bean, gen, provider)
-        } catch (e: Exception) {
-            val name = if (i == props.size) "[anySetter]" else props[i].name
-            wrapAndThrow(provider, e, bean, name)
-        } catch (e: StackOverflowError) {
-            // 04-Sep-2009, tatu: Dealing with this is tricky, since we don't have many
-            //   stack frames to spare... just one or two; can't make many calls.
-
-            // 10-Dec-2015, tatu: and due to above, avoid "from" method, call ctor directly:
-            //JsonMappingException mapE = JsonMappingException.from(gen, "Infinite recursion (StackOverflowError)", e);
-            val mapE = JsonMappingException(gen, "Infinite recursion (StackOverflowError)", e)
-            val name = if (i == props.size) "[anySetter]" else props[i].name
-            mapE.prependPath(JsonMappingException.Reference(bean, name))
-            throw mapE
-        }
-    }
-
-    private fun findImplicitContexts(bean: Any?): HashMap<String, Any?> {
-        val implicitContexts = HashMap<String, Any?>()
-        bean?.let {
-            for (property in bean::class.memberProperties) {
-                property.isAccessible = true
-                property.findAnnotation<ImplicitContext>()?.let {
-                    implicitContexts.put(property.name, resolveMethod(bean, property))
-                }
-            }
-        }
-
-        return implicitContexts
-    }
-
-    private fun resolveMethod(bean: Any?, property: KProperty1<out Any, *>): Any? {
-        val params = property.returnType.javaType as ParameterizedType
-        val inputClass = (params.actualTypeArguments[0] as Class<*>)
-        val fieldDefinition = property.javaField
-        fieldDefinition?.isAccessible = true
-
-        val fieldValue = fieldDefinition?.get(bean)
-        val myMethod = fieldValue?.javaClass?.getDeclaredMethod("invoke", inputClass)
-        myMethod?.isAccessible = true
-
-        val id = generateId(property)
-        val values = inputClass.kotlin.primaryConstructor
-            ?.parameters
-            ?.associate { it to (if (it.name == "contextId") id else null) }
-            ?: error("parameters not found")
-
-        val inputParam = inputClass.kotlin.primaryConstructor?.callBy(values)
-        return myMethod?.invoke(fieldValue, inputParam)
-    }
-
-    private fun generateId(property: KProperty1<out Any, *>): String {
-        return property.findAnnotation<ImplicitContext>()?.id?.let {
-            if (it.isEmpty())
-                property.name
-            else
-                it.replace(" ", "")
-        } ?: run {
-            property.name
-        }
     }
 }
