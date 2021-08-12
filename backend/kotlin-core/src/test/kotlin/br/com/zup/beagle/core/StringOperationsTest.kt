a680ca4554e4c0c81ff1d4633a9a0275be6812b5
Frederico Noce <frederico.noce@zup.com.br>
2021-08-12 16:26:54 -0300

    capitalize operation test

---
+++ backend/kotlin-core/src/test/kotlin/br/com/zup/beagle/core/StringOperationsTest.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.core
+
+import br.com.zup.beagle.context.Bind
+import br.com.zup.beagle.context.constant
+import br.com.zup.beagle.context.operations.builtin.capitalize
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("Given a String Operations")
+internal class StringOperationsTest {
+
+    @DisplayName("When use capitalize operation")
+    @Nested
+    inner class CapitalizeTest {
+
+        @DisplayName("Then should return the bind of string with capitalize operation")
+        @Test
+        fun test_capitalize_operation() = run {
+            val input = constant("capitalize")
+            val expected = Bind.expression<String>("@{capitalize(\'${input.value}\')}")
+
+            val result = capitalize(input)
+
+            Assertions.assertEquals(result, expected)
+        }
+    }
+}
